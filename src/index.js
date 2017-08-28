c143255b197741d9825eec8541abcb942e7708dd
Snigdha Sur <Snigdha@Snigdhas-MacBook-Air-2.local>
2017-08-28 13:33:53 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,36 @@
 // Code Goes Here
+import React from 'react';
+import PropTypes from 'prop-types';
+ 
+class Order extends React.Component {
+ 
+  render() {
+    return (
+      <div className="order">
+        <ul>
+          <li>{this.props.cone ? 'Cone' : 'Cup'}</li>
+          <li>{this.props.size}</li>
+          <li>{this.props.scoops.length} scoops: {this.props.scoops.join(', ')}</li>
+          <li>Ordered by {this.props.orderInfo.customerName} at {this.props.orderInfo.orderedAt}.</li>
+        </ul>
+      </div>
+    );
+  }
+}
+ 
+Order.defaultProps = {
+  cone: true,
+  size: 'regular'
+};
+ 
+Order.propTypes = {
+	cone: PropTypes.bool
+	size: PropTypes.string
+	// scoops: PropTypes.array.isRequired --> this requires array; below requires array of strings
+	scoops: PropTypes.arrayOf(PropTypes.string).isRequired
+	// orderInfo: PropTypes.object.isRequired; below gives specific inputs that are required in the object
+	orderInfo: PropTypes.shape({
+	    customerName: PropTypes.string.isRequired,
+	    orderedAt: PropTypes.number.isRequired // We're using UNIX timestamps here
+  	}).isRequired
+};
